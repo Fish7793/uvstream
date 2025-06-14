f2d15da76d0755cdab6078869d26d3d795e71be8
Fish7793 <daveravab@gmail.com>
2025-06-14 15:18:48 -0400

    [feat] basic polars.DataFrame operations. Removed type aliases

--- uvstream/pl.py
+++ uvstream/pl.py
@@ -1,17 +1,58 @@
 from importlib.util import find_spec
+from typing import Any
+from datetime import timedelta
 
 if not find_spec('polars'):
     raise ModuleNotFoundError(name='polars')
 
 import polars as pl
-
+from polars._typing import IntoExprColumn
 from uvstream.stream import *
 
-PLStream = Stream[pl.DataFrame, pl.DataFrame]
-PLSource = Source[pl.DataFrame]
-PLSink = Sink[pl.DataFrame]
-PLMap = Map[pl.DataFrame, pl.DataFrame]
-PLZip = Zip[pl.DataFrame]
-PLFilter = Filter[pl.DataFrame]
-PLWindow = Window[pl.DataFrame]
-PLUnpack = Unpack[pl.DataFrame]
+PLPredicate = IntoExprColumn | Iterable[IntoExprColumn] | bool | list[bool] | Any
+
+class PLStream(Stream[pl.DataFrame, pl.DataFrame]):
+
+    def __init__(self, upstream = None, fn = None, name = None, *args, **kwargs):
+        super().__init__(upstream, fn, name, *args, **kwargs)
+
+    def pl_filter(self, *predicates:PLPredicate, **constraints:Any) -> 'PLStream':
+        return PLStream(self, lambda x: x.filter(*predicates, **constraints))
+    
+    def pl_select(self, *predicates:PLPredicate, **constraints:Any) -> 'PLStream':
+        return PLStream(self, lambda x: x.select(*predicates, **constraints))
+    
+    def pl_with_columns(self, *predicates:PLPredicate, **constraints:Any) -> 'PLStream':
+        return PLStream(self, lambda x: x.with_columns(*predicates, **constraints))
+    
+    def pl_insert_column(self, index:int, column:IntoExprColumn) -> 'PLStream':
+        return PLStream(self, lambda x: x.insert_column(index, column))
+
+
+class PLFuncWindow(PLStream):
+    def __init__(self, upstream=None, fn=None, name=None, *args, **kwargs):
+        super().__init__(upstream, fn, name, *args, **kwargs)
+        self._buffer:pl.DataFrame = None
+
+    def _update_buffer(self, x:pl.DataFrame, *predicates:PLPredicate):
+        if self._buffer:
+            self._buffer = self._buffer.extend(x)
+        else:
+            self._buffer = x.clone()
+        self._buffer = self._buffer.filter(predicates)
+
+
+
+class PLTimeWindow(PLFuncWindow):
+
+    def __init__(self, upstream=None, t:timedelta|float=0.0, time_column:IntoExprColumn=pl.col('ts'), *args, **kwargs):
+        super().__init__(upstream, None, *args, **kwargs)
+        if isinstance(t, timedelta):
+            t = t.total_seconds()
+        self.t = t
+        self.time_column = time_column
+
+
+    async def update(self, x:pl.DataFrame, who:PLStream):
+        self._update_buffer(x, (pl.lit(datetime.now()) - self.time_column).dt.total_seconds() < self.t)
+        self(self._buffer)
