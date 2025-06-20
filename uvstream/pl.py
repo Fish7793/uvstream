f609dc51bd37e63761d3f84909f2136bf45d462a
Fish7793 <asyncfish@proton.me>
2025-06-19 22:54:41 -0400

    [milestone] generally usable, exception handling for tasks created by Stream.__call__, PLFuncWindow, PLTimeWindow

--- uvstream/pl.py
+++ uvstream/pl.py
@@ -16,37 +16,48 @@ class PLStream(Stream[pl.DataFrame, pl.DataFrame]):
     def __init__(self, upstream = None, fn = None, name = None, *args, **kwargs):
         super().__init__(upstream, fn, name, *args, **kwargs)
 
-    def pl_filter(self, *predicates:PLPredicate, **constraints:Any) -> 'PLStream':
-        return PLStream(self, lambda x: x.filter(*predicates, **constraints))
+    def pl_filter(self, *predicates:PLPredicate, name=None, **constraints:Any) -> 'PLStream':
+        return PLStream(self, lambda x: x.filter(*predicates, **constraints), name=name)
     
-    def pl_select(self, *predicates:PLPredicate, **constraints:Any) -> 'PLStream':
-        return PLStream(self, lambda x: x.select(*predicates, **constraints))
+    def pl_map(self, fn:Callable[[pl.DataFrame], pl.DataFrame]) -> 'PLStream':
+        return PLStream(self, fn)
+
+    def pl_select(self, *predicates:PLPredicate, name=None, **constraints:Any) -> 'PLStream':
+        return PLStream(self, lambda x: x.select(*predicates, **constraints), name=name)
     
-    def pl_with_columns(self, *predicates:PLPredicate, **constraints:Any) -> 'PLStream':
-        return PLStream(self, lambda x: x.with_columns(*predicates, **constraints))
+    def pl_with_columns(self, *predicates:PLPredicate, name=None, **constraints:Any) -> 'PLStream':
+        return PLStream(self, lambda x: x.with_columns(*predicates, **constraints), name=name)
     
-    def pl_insert_column(self, index:int, column:IntoExprColumn) -> 'PLStream':
-        return PLStream(self, lambda x: x.insert_column(index, column))
+    def pl_insert_column(self, index:int, column:IntoExprColumn, name=None) -> 'PLStream':
+        return PLStream(self, lambda x: x.insert_column(index, column), name=name)
 
 
 class PLFuncWindow(PLStream):
-    def __init__(self, upstream=None, fn=None, name=None, *args, **kwargs):
+    def __init__(self, upstream=None, fn=None, name=None, min_samples:int=0, unique_col:Optional[IntoExprColumn]=None, *args, **kwargs):
         super().__init__(upstream, fn, name, *args, **kwargs)
         self._buffer:pl.DataFrame = None
+        self.min_samples = min_samples
+        self.unique_col = unique_col
+        self.on_append = Event[Any, None]()
 
-    def _update_buffer(self, x:pl.DataFrame, *predicates:PLPredicate):
-        if self._buffer:
-            self._buffer = self._buffer.extend(x)
+    def _update_buffer(self, x:pl.DataFrame, *predicates:PLPredicate, **constraints:Any):
+        if self._buffer is not None:
+            if self.unique_col is not None:
+                x = x.filter(self.unique_col.is_in(self._buffer.select(self.unique_col).to_series()).not_())
+            self._buffer = self._buffer.vstack(x)
         else:
             self._buffer = x.clone()
-        self._buffer = self._buffer.filter(predicates)
+
+        self.on_append(self._buffer, x)
+        if len(self._buffer) > self.min_samples:
+            self._buffer = self._buffer.filter(*predicates, **constraints)
 
 
-
+@register_stream(PLStream)
 class PLTimeWindow(PLFuncWindow):
 
     def __init__(self, upstream=None, t:timedelta|float=0.0, time_column:IntoExprColumn=pl.col('ts'), *args, **kwargs):
-        super().__init__(upstream, None, *args, **kwargs)
+        super().__init__(upstream, *args, **kwargs)
         if isinstance(t, timedelta):
             t = t.total_seconds()
         self.t = t
@@ -54,5 +65,5 @@ class PLTimeWindow(PLFuncWindow):
 
 
     async def update(self, x:pl.DataFrame, who:PLStream):
-        self._update_buffer(x, (pl.lit(datetime.now()) - self.time_column).dt.total_seconds() < self.t)
-        self(self._buffer)
+        self._update_buffer(x, ((pl.lit(datetime.now()) - self.time_column).dt.total_seconds()) < self.t)
+        await self(self._buffer)
